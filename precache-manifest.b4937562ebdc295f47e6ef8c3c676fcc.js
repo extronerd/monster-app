ed56256973d174ed63efd44064e6638fa2de58de
lennysoft90 <ogu4leo@gmail.com>
2019-12-27 12:43:29 +0100

    Updates

---
+++ precache-manifest.b4937562ebdc295f47e6ef8c3c676fcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a7fec096294dbe32f3e4b20d882031",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "c9837ee7de97e4fafe4f",
+    "url": "/monster-rolodex/static/css/main.7123b3b2.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9837ee7de97e4fafe4f",
+    "url": "/monster-rolodex/static/js/main.573cc329.chunk.js"
+  },
+  {
+    "revision": "23fb47eb6f86565db1b0",
+    "url": "/monster-rolodex/static/js/runtime-main.47b0ddc3.js"
+  }
+]);
